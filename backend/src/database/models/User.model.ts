604ed516157ff8cbbe475808580f6374f854dffb
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-05-20 09:15:43 -0300

    feat: user model

---
+++ backend/src/database/models/User.model.ts
@@ -0,0 +1,50 @@
+import { DataTypes, Model } from 'sequelize';
+import sequelize from '.';
+
+export interface UserAttributes {
+  id: number;
+  username: string;
+  email: string;
+  password: string;
+}
+
+export type UserCreateAttr = Omit<UserAttributes, 'id'>;
+
+class UserModel extends Model<UserAttributes, UserCreateAttr> {
+  declare id: number;
+  declare username: string;
+  declare email: string;
+  declare password: string;
+}
+
+UserModel.init(
+  {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    username: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    email: {
+      allowNull: false,
+      unique: true,
+      type: DataTypes.STRING,
+    },
+    password: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    tableName: 'users',
+    sequelize,
+    timestamps: false,
+    underscored: true,
+  },
+);
+
+export default UserModel;
