78590b62660569ef435b8fabfc1e1fda8c062c22
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-05-22 15:22:03 -0300

    refactory: var to const

--- app/backend/src/database/config/database.js
+++ app/backend/src/database/config/database.js
@@ -1,6 +1,6 @@
 "use strict";
 exports.__esModule = true;
-var config = {
+const config = {
     username: process.env.DB_USER || 'root',
     password: process.env.DB_PASS || '123456',
     database: 'RECIPES_APP',
