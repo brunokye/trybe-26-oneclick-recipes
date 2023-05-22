ae862352e91ab075faa18f424089761a062e2bfa
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-05-22 15:48:25 -0300

    feat: redirect case token in local storage

--- app/frontend/src/pages/Home.jsx
+++ app/frontend/src/pages/Home.jsx
@@ -1,11 +1,22 @@
-import {Link} from  'react-router-dom';
+import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import { readObject } from '../helpers';
 
 export default function Home() {
-  return(
+  const history = useHistory();
+
+  useEffect(() => {
+    if (readObject('token', null)) {
+      history.push('/meals');
+    }
+  }, [history]);
+
+  return (
     <>
       <h1>Bem vindo ao One Click Recipes</h1>
-      <button><Link to='user/login'>Login</Link></button>
-      <button><Link to='user/cadastro'>Cadastrar-se</Link></button>
+      <button><Link to="login">Login</Link></button>
+      <button><Link to="cadastro">Cadastrar-se</Link></button>
     </>
   );
 }
