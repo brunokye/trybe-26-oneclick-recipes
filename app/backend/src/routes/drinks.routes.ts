dd191694513c8991673435d1e3f6bb2007468440
Bruno Ferreira <contatobrunokye@gmail.com>
2023-05-22 15:43:29 -0300

    refactor: routes organization

--- app/backend/src/routes/drinks.routes.ts
+++ app/backend/src/routes/drinks.routes.ts
@@ -3,20 +3,19 @@ import DrinkController from '../controllers/Drink.controller';
 
 const drinksRouter = Router();
 
-// query q=nomes
-drinksRouter.get('/name', DrinkController.findByName);
+const { findByName, findByFirstLetter, findRandom,
+  findAllCategories, findByIngredient, findByCategory } = DrinkController;
 
-// query q=primeira-letra
-drinksRouter.get('/letter', DrinkController.findByFirstLetter);
-
-drinksRouter.get('/random', DrinkController.findRandom);
-
-drinksRouter.get('/categories', DrinkController.findAllCategories);
-
-// query q=ingredient-name
-drinksRouter.get('/ingredient', DrinkController.findByIngredient);
-
-// query q=category-name
-drinksRouter.get('/category', DrinkController.findByCategory);
+drinksRouter
+  // query q=nomes
+  .get('/name', findByName)
+  // query q=primeira-letra
+  .get('/letter', findByFirstLetter)
+  .get('/random', findRandom)
+  .get('/categories', findAllCategories)
+  // query q=ingredient-name
+  .get('/ingredient', findByIngredient)
+  // query q=category-name
+  .get('/category', findByCategory);
 
 export default drinksRouter;
