5426cceb24085b9d9618935799ff6766396b0339
Bruno Ferreira <contatobrunokye@gmail.com>
2023-05-22 13:33:36 -0300

    feat: drinks controller

---
+++ app/backend/src/controllers/Drink.controller.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from 'express';
+import DrinkService from '../services/Drink.service';
+
+export default class DrinkController {
+  public static async findByName(req: Request, res: Response) {
+    const { q = '' } = req.query;
+    const drinks = await DrinkService.findByName(q as string);
+
+    res.status(200).json({ drinks });
+  }
+
+  public static async findByFirstLetter(req: Request, res: Response) {
+    const { q = '' } = req.query;
+    const drinks = await DrinkService.findByFirstLetter(q as string);
+
+    res.status(200).json({ drinks });
+  }
+
+  public static async findByCategory(req: Request, res: Response) {
+    const { q = '' } = req.query;
+    const drinks = await DrinkService.findByCategory(q as string);
+
+    res.status(200).json({ drinks });
+  }
+
+  public static async findByIngredient(req: Request, res: Response) {
+    const { q = '' } = req.query;
+    const drinks = await DrinkService.findByIngredient(q as string);
+
+    res.status(200).json({ drinks });
+  }
+
+  public static async findRandom(req: Request, res: Response) {
+    const drinks = await DrinkService.findRandom();
+
+    res.status(200).json({ drinks });
+  }
+}
