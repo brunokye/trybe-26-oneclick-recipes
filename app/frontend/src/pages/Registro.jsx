ea58363f54467c17454c1a21a8fa9a9a2083a475
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-05-22 15:35:29 -0300

    feat: form de registro

--- app/frontend/src/pages/Registro.jsx
+++ app/frontend/src/pages/Registro.jsx
@@ -1,5 +1,86 @@
-export default function Regiter() {
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import { isValidEmail, isValidPassword, isValidUsername } from '../helpers';
+// import { saveUser } from '../services/userLS';
+import { requestLogin } from '../helpers/fetch';
+import '../styles/login.css';
+
+export default function Registro() {
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [disabled, setDisabled] = useState(true);
+  const [fetchError, setfetchError] = useState(false);
+  const [fetchMessage, setFetchMessage] = useState('test');
+
+  const history = useHistory();
+
+  const doLogin = async () => {
+    const data = await requestLogin('/users/register', {
+      email,
+      password,
+      username,
+    });
+    if (data.message) {
+      setfetchError(true);
+      setFetchMessage(data.message);
+    } else {
+      history.push('/meals');
+    }
+  };
+
+  useEffect(() => {
+    const isEmailValid = isValidEmail(email);
+    const isPasswordValid = isValidPassword(password);
+    const isUsernameValid = isValidUsername(username);
+    const isDisabled = !(isEmailValid && isPasswordValid && isUsernameValid);
+    setDisabled(isDisabled);
+  }, [email, password, username]);
+
   return (
-    <h1>Regisrto</h1>
+    <div className="container">
+      <h1>{fetchError && fetchMessage}</h1>
+      <h4 className="titleLogin">oneClick Recipes</h4>
+      <div className="loginContainer">
+        <h6>Login</h6>
+        <label htmlFor="username">
+          <input
+            placeholder="Nome"
+            type="text"
+            id="username"
+            onChange={ ({ target: { value } }) => setUsername(value) }
+            value={ username }
+          />
+        </label>
+        <label htmlFor="email">
+          <input
+            placeholder="e-mail"
+            type="email"
+            id="email"
+            data-testid="email-input"
+            onChange={ ({ target: { value } }) => setEmail(value) }
+            value={ email }
+          />
+        </label>
+        <label htmlFor="password">
+          <input
+            placeholder="Password"
+            type="password"
+            id="password"
+            data-testid="password-input"
+            onChange={ ({ target: { value } }) => setPassword(value) }
+            value={ password }
+          />
+        </label>
+        <button
+          type="button"
+          data-testid="login-submit-btn"
+          onClick={ doLogin }
+          disabled={ disabled }
+        >
+          Login
+        </button>
+      </div>
+    </div>
   );
 }
