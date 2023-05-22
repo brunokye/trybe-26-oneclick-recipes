088aaa5db1f9ad3357699e2ab724097ce3fc4225
Bruno Ferreira <contatobrunokye@gmail.com>
2023-05-22 13:33:56 -0300

    feat: drinks router

--- app/backend/src/routes/drinks.routes.ts
+++ app/backend/src/routes/drinks.routes.ts
@@ -1,23 +1,20 @@
 import { Router } from 'express';
+import DrinkController from '../controllers/Drink.controller';
 
 const drinksRouter = Router();
 
 // query q=nomes
-drinksRouter.get('/name', () => {});
+drinksRouter.get('/name', DrinkController.findByName);
 
 // query q=primeira-letra
-drinksRouter.get('/letter', () => {});
+drinksRouter.get('/letter', DrinkController.findByFirstLetter);
 
-drinksRouter.get('/random', () => {});
-
-drinksRouter.get('/categories', () => {});
-
-drinksRouter.get('/ingredients', () => {});
+drinksRouter.get('/random', DrinkController.findRandom);
 
 // query q=ingredient-name
-drinksRouter.get('/ingredient', () => {});
+drinksRouter.get('/ingredient', DrinkController.findByIngredient);
 
 // query q=category-name
-drinksRouter.get('/category', () => {});
+drinksRouter.get('/category', DrinkController.findByCategory);
 
 export default drinksRouter;
