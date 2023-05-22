959c94dffa7e9019b843aa8e148db7e9dc2f17a9
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-05-22 16:50:53 -0300

    refactory: meat service

--- app/backend/src/services/Meal.service.ts
+++ app/backend/src/services/Meal.service.ts
@@ -3,74 +3,63 @@ import MealModel from '../database/models/Meal.model';
 
 export default class MealService {
   public static async findByName(name: string) {
-    const meals = await MealModel.findAll({
+    return MealModel.findAll({
       where: {
         strMeal: {
           [Op.like]: `%${name}%`,
         },
       },
     });
-    return meals;
   }
 
   public static async findByFirstLetter(letter: string) {
-    const meals = await MealModel.findAll({
+    return MealModel.findAll({
       where: {
         strMeal: {
           [Op.like]: `${letter}%`,
         },
       },
     });
-    return meals;
   }
 
   public static async findByCategory(category: string) {
-    const meals = await MealModel.findAll({
+    return MealModel.findAll({
       where: {
         strCategory: category,
       },
     });
-
-    return meals;
   }
 
   public static async findByArea(area: string) {
-    const meals = await MealModel.findAll({
+    return MealModel.findAll({
       where: {
         strArea: area,
       },
       attributes: ['strMeal', 'strMealThumb', 'idMeal'],
     });
-
-    return meals;
   }
 
   public static async findRandom() {
-    const meals = await MealModel.findAll({
+    return MealModel.findAll({
       order: Sequelize.literal('RAND()'),
       limit: 1,
     });
-
-    return meals;
   }
 
   public static async findAllAreas() {
-    const areas = await MealModel.findAll({
+    return MealModel.findAll({
       attributes: [
         [Sequelize.fn('DISTINCT', Sequelize.col('strArea')), 'strArea'],
       ],
       order: [['strArea', 'ASC']],
     });
-    return areas;
   }
 
   public static async findByIngredient(ingredient: string) {
-    const meals = await MealModel.findAll({
+    return MealModel.findAll({
       where: {
         strIngredient1: ingredient,
       },
     });
-
-    return meals;
   }
 }
--- app/backend/src/services/MealCategory.service.ts
+++ app/backend/src/services/MealCategory.service.ts
@@ -2,7 +2,6 @@ import MealCategoryModel from '../database/models/MealCategory.model';
 
 export default class MealCategoryService {
   public static async findAll() {
-    const meals = await MealCategoryModel.findAll();
-    return meals;
+    return MealCategoryModel.findAll();
   }
 }
