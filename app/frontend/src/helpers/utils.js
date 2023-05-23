490d3d847ef8b61fcf4838b6130280ad1648d7f8
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-05-23 15:15:42 -0300

    feat: construtor do body para finish

--- app/frontend/src/helpers/utils.js
+++ app/frontend/src/helpers/utils.js
@@ -1,8 +1,16 @@
 export const parseJSONResponse = async (response, defaultValue) => {
   try {
-    const responseJSON = await response.json();
-    return responseJSON;
+    return await response.json();
   } catch {
     return defaultValue;
   }
 };
+
+export const finishConstructor = (recipe) => ({
+  category: recipe.strCategory,
+  alcoholicOrNot: recipe.alcoholicOrNot || '',
+  name: recipe.strMeal || recipe.strDrink,
+  image: recipe.strMealThumb || recipe.strDrinkThumb,
+  nationality: recipe.strArea || '',
+  tags: recipe.strTags,
+});
