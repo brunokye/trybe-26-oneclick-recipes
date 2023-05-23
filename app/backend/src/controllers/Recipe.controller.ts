1df2397a3bbd9b89631f917dce9f31178bf90836
brunaCFreitas <bruna_c_freitas@hotmail.com>
2023-05-23 18:39:36 -0300

    feat: cria tabela e endpoints de receitas favoritas

--- app/backend/src/controllers/Recipe.controller.ts
+++ app/backend/src/controllers/Recipe.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import MealRecipeService from '../services/MealRecipe.service';
 import { RecipeDone } from '../dtos/recipe/recipeDone.dto';
 import RecipesDoneService from '../services/RecipesDone.service';
+import RecipesFavovitesService from '../services/RecipesFavorite.service';
 
 export default class RecipeController {
   public static async getMealRecipeInProgress(req: Request, res: Response) {
@@ -48,5 +49,37 @@ export default class RecipeController {
     const recipes = await RecipesDoneService
       .getFinishedMealRecipes(idUser as string, type as string);
     res.status(200).json(recipes);
+  }
+
+  public static async getFavoritesRecipes(req: Request, res: Response) {
+    const { idUser } = req.headers;
+    const { type } = req.query;
+
+    const recipesFavorites = await RecipesFavovitesService
+      .getFavoritesRecipes(idUser as string, type as string);
+    res.status(200).json(recipesFavorites);
+  }
+
+  public static async addFavoriteRecipe(req: Request, res: Response) {
+    const { idUser } = req.headers;
+    const { type } = req.query;
+
+    const recipe = {
+      idUser: Number(idUser),
+      ...req.body,
+      type: type as string,
+    } as RecipeDone;
+
+    await RecipesFavovitesService.addFavoriteRecipe(recipe);
+    res.status(200).json({ message: 'ok' });
+  }
+
+  public static async removeFavoriteRecipe(req: Request, res: Response) {
+    const { idUser } = req.headers;
+    const { idRecipe } = req.params;
+    const { type } = req.query;
+
+    await RecipesFavovitesService.removeFavoriteRecipe(idUser as string, idRecipe, type as string);
+    res.status(200).json({ message: 'ok' });
   }
 }
---
+++ app/backend/src/dtos/recipe/recipeFavorite.dto.ts
@@ -0,0 +1,9 @@
+export type RecipeFavorite = {
+  idUser: number;
+  idRecipe: string;
+  category: string;
+  alcoholicOrNot: string;
+  name: string;
+  image: string;
+  nationality: string;
+};
