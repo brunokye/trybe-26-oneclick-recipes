471fafbe7a30f4023a08e0ee60b3662e62c0eb53
Bruno Ferreira <contatobrunokye@gmail.com>
2023-05-25 15:01:02 -0300

    test: auth

--- app/backend/src/tests/mocks/auth.mock.ts
+++
@@ -1,5 +0,0 @@
-export const authorization = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImVtYWlsIjoidXNlckB1c2VyLmNvbSJ9LCJpYXQiOjE2ODQ0MzA3NDksImV4cCI6MTY4NTAzNTU0OX0.Z2whljyqpT50VNoBS9-u8q7a6upRAmxp8R7tsadjfS0'
-
-export const token = {
-  'token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImVtYWlsIjoidXNlckB1c2VyLmNvbSJ9LCJpYXQiOjE2ODQ0MzA3NDksImV4cCI6MTY4NTAzNTU0OX0.Z2whljyqpT50VNoBS9-u8q7a6upRAmxp8R7tsadjfS0'
-}
