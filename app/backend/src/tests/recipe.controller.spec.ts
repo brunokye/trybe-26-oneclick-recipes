3fa12b61b8056b65b1f19afc28e8377243b78625
Bruno Ferreira <contatobrunokye@gmail.com>
2023-05-25 15:01:34 -0300

    test: recipe controller

--- app/backend/src/tests/recipe.controller.spec.ts
+++ app/backend/src/tests/recipe.controller.spec.ts
@@ -16,10 +16,9 @@ import RecipesFavoritesService from '../services/RecipesFavorite.service';
 import {
   getDrinkStarted, getMealStarted,
   updateDrink, updateMeal,
-  finishDrink, finishMeal, doneRecipe,
+  bodyFinishDrink, bodyFinishMeal, doneRecipe,
   createFavoriteRecipe, removeFavoriteRecipe, getAllFavorites,
 } from './mocks/recipe.mock';
-import { authorization, token } from './mocks/auth.mock';
 
 import 'mocha';
 
@@ -27,7 +26,7 @@ const { expect } = chai;
 
 chai.use(chaiHttp);
 
-describe('DrinkController', () => {
+describe('RecipeController', () => {
   let chaiHttpResponse: Response;
 
   afterEach(() => {
@@ -37,20 +36,190 @@ describe('DrinkController', () => {
   describe('getRecipeInProgress', () => {
     it('retorna as informações para dar inicio a receita do drink', async () => {
       // @ts-ignore
-      sinon.stub(DrinkRecipe, 'getDrinkRecipeInProgress').resolves(null);
-      // @ts-ignore
-      sinon.stub(DrinkRecipe, 'updateDrinkRecipeInProgress').resolves(updateDrink);
-      // @ts-ignore
-      sinon.stub(jwt, 'verifyToken').resolves(token)
+      sinon.stub(DrinkRecipe, 'getDrinkRecipeInProgress').resolves(updateDrink);
+      sinon.stub(jwt, 'verifyToken').returns({ id: 1, message: 'test' })
 
       chaiHttpResponse = await chai.request(app)
-        .get('/recipes/drinks/in-progress/15997')
-        .set('Authorization', authorization);
+        .get('/recipes/drinks/in-progress/15997');
 
       const { status, body } = chaiHttpResponse
 
       expect(status).to.be.equal(200);
       expect(body).to.be.deep.equal(updateDrink);
+    });
+
+    it('retorna as informações para dar inicio a receita da meal', async () => {
+      // @ts-ignore
+      sinon.stub(MealRecipe, 'getMealRecipeInProgress').resolves(updateMeal);
+      sinon.stub(jwt, 'verifyToken').returns({ id: 1, message: 'test' })
+
+      chaiHttpResponse = await chai.request(app)
+        .get('/recipes/meals/in-progress/52978');
+
+      const { status, body } = chaiHttpResponse
+
+      expect(status).to.be.equal(200);
+      expect(body).to.be.deep.equal(updateMeal);
+    })
+  })
+
+  describe('updateRecipeInProgress', () => {
+    it('retorna a confirmação de atualização do drink em progresso', async () => {
+      // @ts-ignore
+      sinon.stub(DrinkRecipe, 'updateDrinkRecipeInProgress').resolves(getDrinkStarted);
+      sinon.stub(jwt, 'verifyToken').returns({ id: 1, message: 'test' })
+
+      chaiHttpResponse = await chai.request(app)
+        .patch('/recipes/drinks/in-progress/15997');
+
+      const { status, body } = chaiHttpResponse
+
+      expect(status).to.be.equal(200);
+      expect(body).to.be.deep.equal({ message: 'ok' });
+    });
+
+    it('retorna a confirmação de atualização da meal em progresso', async () => {
+      // @ts-ignore
+      sinon.stub(MealRecipe, 'updateMealRecipeInProgress').resolves(getMealStarted);
+      sinon.stub(jwt, 'verifyToken').returns({ id: 1, message: 'test' })
+
+      chaiHttpResponse = await chai.request(app)
+        .patch('/recipes/meals/in-progress/52978');
+
+      const { status, body } = chaiHttpResponse
+
+      expect(status).to.be.equal(200);
+      expect(body).to.be.deep.equal({ message: 'ok' });
+    });
+  })
+
+
+  describe('finishRecipeInProgress', () => {
+    it('retorna a confirmação de finalização do drink em progresso', async () => {
+      // @ts-ignore
+      const finishDrink = sinon.stub(DrinkRecipe, 'finishDrinkRecipeInProgress');
+      sinon.stub(jwt, 'verifyToken').returns({ id: 1, message: 'test' })
+
+      chaiHttpResponse = await chai.request(app)
+        .post('/recipes/in-progress/15997/finish')
+        .send(bodyFinishDrink);
+
+      const { status, body } = chaiHttpResponse
+
+      // @ts-ignore
+      expect(finishDrink.calledWith({
+        ...bodyFinishDrink,
+        idUser: 1,
+        idRecipe: '15997'
+      })).to.be.true;
+      expect(status).to.be.equal(200);
+      expect(body).to.be.deep.equal({ message: 'ok' });
+    });
+
+    it('retorna a confirmação de finalização da meal em progresso', async () => {
+      // @ts-ignore
+      const finishMeal = sinon.stub(MealRecipe, 'finishMealRecipeInProgress');
+      sinon.stub(jwt, 'verifyToken').returns({ id: 1, message: 'test' })
+
+      chaiHttpResponse = await chai.request(app)
+        .post('/recipes/in-progress/52978/finish')
+        .send(bodyFinishMeal);
+
+      const { status, body } = chaiHttpResponse
+
+      // @ts-ignore
+      expect(finishMeal.calledWith({
+        ...bodyFinishMeal,
+        idUser: 1,
+        idRecipe: '52978'
+      })).to.be.true;
+      expect(status).to.be.equal(200);
+      expect(body).to.be.deep.equal({ message: 'ok' });
+    });
+
+    it('retorna as receitas finalizadas', async () => {
+      // @ts-ignore
+      sinon.stub(RecipesDoneService, 'getFinishedRecipes').resolves(doneRecipe);
+      sinon.stub(jwt, 'verifyToken').returns({ id: 1, message: 'test' })
+
+      chaiHttpResponse = await chai.request(app)
+        .get('/recipes/done');
+
+      const { status, body } = chaiHttpResponse
+
+      expect(status).to.be.equal(200);
+      expect(body).to.be.deep.equal(doneRecipe);
+    });
+  })
+
+  describe('favoriteRecipe', () => {
+    it('adiciona a receita aos favoritos', async () => {
+      // @ts-ignore
+      sinon.stub(RecipesFavoritesService, 'addFavoriteRecipe').resolves(createFavoriteRecipe);
+      sinon.stub(jwt, 'verifyToken').returns({ id: 1, message: 'test' })
+
+      chaiHttpResponse = await chai.request(app)
+        .post('/recipes/favorites/15997');
+
+      const { status, body } = chaiHttpResponse
+
+      expect(status).to.be.equal(200);
+      expect(body).to.be.deep.equal({ message: 'ok' });
+    });
+
+    it('remove a receita dos favoritos', async () => {
+      // @ts-ignore
+      sinon.stub(RecipesFavoritesService, 'removeFavoriteRecipe').resolves(removeFavoriteRecipe);
+      sinon.stub(jwt, 'verifyToken').returns({ id: 1, message: 'test' })
+
+      chaiHttpResponse = await chai.request(app)
+        .delete('/recipes/favorites/15997');
+
+      const { status, body } = chaiHttpResponse
+
+      expect(status).to.be.equal(200);
+      expect(body).to.be.deep.equal({ message: 'ok' });
+    });
+
+    it('retorna todos os favoritos', async () => {
+      // @ts-ignore
+      sinon.stub(RecipesFavoritesService, 'getFavoritesRecipes').resolves(getAllFavorites);
+      sinon.stub(jwt, 'verifyToken').returns({ id: 1, message: 'test' })
+
+      chaiHttpResponse = await chai.request(app)
+        .get('/recipes/favorites');
+
+      const { status, body } = chaiHttpResponse
+
+      expect(status).to.be.equal(200);
+      expect(body).to.be.deep.equal(getAllFavorites);
+    });
+  })
+
+  describe('error', () => {
+    it('retorna a mensagem de token invalido', async () => {
+      chaiHttpResponse = await chai.request(app)
+        .get('/recipes/drinks/in-progress/15997')
+        .set('Authorization', 'token');
+
+      const { status, body } = chaiHttpResponse
+
+      expect(status).to.be.equal(401);
+      expect(body).to.be.deep.equal({ message: 'Token must be a valid token' });
+    });
+
+    it('retorna status de internal server error', async () => {
+      // @ts-ignore
+      sinon.stub(DrinkRecipe, 'getDrinkRecipeInProgress').rejects();
+      sinon.stub(jwt, 'verifyToken').returns({ id: 1, message: 'test' })
+
+      chaiHttpResponse = await chai.request(app)
+        .get('/recipes/drinks/in-progress/15997')
+        .set('Authorization', 'token');
+
+      const { status, body } = chaiHttpResponse
+
+      expect(status).to.be.equal(500);
     });
   })
 });
